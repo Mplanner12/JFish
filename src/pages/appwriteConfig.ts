ddf3cf8138f24c8c365dae1b300555b4f3fedcda
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2024-05-16 14:26:41 +0100

    config endpoints

--- src/pages/appwriteConfig.ts
+++ src/pages/appwriteConfig.ts
@@ -4,18 +4,10 @@ const client = new Client();
 
 client
   .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('65fe59b5408c35449512');
+  .setProject('6645ddbf001119e1023a');
 
 // Export Account
 export const account = new Account(client);
 
 // Export the Appwrite client
 export default client;
-
-// Set CORS headers for all Appwrite API requests
-// appwrite.setHeaders({
-//   "Access-Control-Allow-Origin":
-//     "https://edas-p3fcy9xhd-mplanner12s-projects.vercel.app",
-//   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-//   "Access-Control-Allow-Headers": "Content-Type, Authorization",
-// });
